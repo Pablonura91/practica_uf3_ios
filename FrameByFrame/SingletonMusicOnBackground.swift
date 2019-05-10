942ab4512ea8a8107588702b0585cf7a897b1f10
pablonura91 <pablonura91@gmail.com>
2019-05-10 07:11:11 +0200

    add music background

---
+++ FrameByFrame/SingletonMusicOnBackground.swift
@@ -0,0 +1,49 @@
+//
+//  SingletonMusicOnBackground.swift
+//  FrameByFrame
+//
+//  Created by Pablo Nuñez on 10/05/2019.
+//  Copyright © 2019 CFGS La Salle Gracia. All rights reserved.
+//
+
+
+import Foundation
+import AVFoundation
+
+class SingletonMusicOnBackground {
+    static let sharedInstance = SingletonMusicOnBackground()
+    private var player: AVAudioPlayer?
+    
+    func create(){
+        do {
+            try AVAudioSession.sharedInstance().setActive(true)
+        }catch {
+            print("Settings")
+        }
+        
+        if let backgroundURL = Bundle.main.url(forResource: "soundBackground", withExtension: "mp3") {
+            do {
+                player = try AVAudioPlayer(contentsOf: backgroundURL)
+                player?.numberOfLoops = Int(-1)
+                player?.prepareToPlay()
+                //player.play()
+                
+            } catch {
+                print("Negro")
+            }
+        }
+    }
+    
+    func play() {
+        player?.play()
+    }
+    
+    func pause() {
+        player?.pause()
+    }
+    
+    func stop() {
+        player?.stop()
+    }
+}
+
